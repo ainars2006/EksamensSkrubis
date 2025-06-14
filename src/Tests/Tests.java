2da0da48c81d9966ce25f085553eea1c833b2cc6
Skrubis <ainarspro06@gmail.com>
2025-06-14 13:04:13 +0300

    Uztaisīts testa sākuma logs

--- src/Tests/Tests.java
+++ src/Tests/Tests.java
@@ -1,10 +1,32 @@
 package Tests;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 public class Tests {
-
 	public static void main(String[] args) {
-		
+		UIManager.put("OptionPane.background", new java.awt.Color(173, 216, 230));
+        UIManager.put("Panel.background", new java.awt.Color(173, 216, 230));
+        UIManager.put("OptionPane.messageForeground", java.awt.Color.BLACK);
 
+        while (true) {
+            String[] options = {"Pildīt testu", "Aizvērt"};
+            int choice = JOptionPane.showOptionDialog(
+                    null,
+                    "Izvēlies:",
+                    "Tests",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    options,
+                    options[0]
+            );
+            if (choice == 0) {
+            	
+            } else {
+                JOptionPane.showMessageDialog(null, "Uzredzēšanos");
+                System.exit(0);
+            }
+        }
 	}
-
 }
